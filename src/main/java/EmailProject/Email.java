7ce060b049bd689de4c38bcb87c216dadc5f52d7
Danila Aldanov <danilaaldanov73@gmail.com>
2023-01-05 11:36:19 +0300

    added objects in Email class

--- src/main/java/EmailProject/Email.java
+++ src/main/java/EmailProject/Email.java
@@ -1,5 +1,22 @@
 package EmailProject;
 
 public class Email {
+    private String firstName;
+    private String secondName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
 
+    //Constructor to receive first name and second name of user
+
+    //What is department
+
+    //Random password
+
+    //Set mailbox capacity
+
+    //Set the alternate Email
+
+    //Change the password
 }
