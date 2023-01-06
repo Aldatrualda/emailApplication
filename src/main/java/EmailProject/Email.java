a69ff4212b5a2f4aeeaafc795a314361a02ca7df
Danila Aldanov <danilaaldanov73@gmail.com>
2023-01-06 12:10:10 +0300

    added a method to set up a deprtment of a user

--- src/main/java/EmailProject/Email.java
+++ src/main/java/EmailProject/Email.java
@@ -1,5 +1,7 @@
 package EmailProject;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String secondName;
@@ -9,8 +11,37 @@ public class Email {
     private String alternateEmail;
 
     //Constructor to receive first name and second name of user
+    public Email(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
 
-    //What is department
+        //Call a method asking for the department - return the department
+        this.department = setDepartment();
+    }
+
+    //Method to setting up the department
+    private String setDepartment() {
+        System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None" +
+                "\nDepartment code: ");
+        Scanner sc = new Scanner(System.in);
+        int whatDepartmentIs = sc.nextInt();
+        String department = null;
+        switch (whatDepartmentIs) {
+            case (1):
+                department = "Sales";
+                break;
+            case (2):
+                department = "Development";
+                break;
+            case (3):
+                department = "Accounting";
+                break;
+            case (0):
+                department = "";
+                break;
+        }
+        return department;
+    }
 
     //Random password
 
--- src/main/java/EmailProject/Main.java
+++ src/main/java/EmailProject/Main.java
@@ -2,6 +2,6 @@ package EmailProject;
 
 public class Main {
     public static void main(String[] args) {
-
+        Email user1 = new Email("Danila", "Aldanov");
     }
 }
